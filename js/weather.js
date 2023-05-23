1cd8a1f16ae712b6ba24b7e8d3578f22634392e9
TResuni <3026946953@qq.com>
2023-05-23 22:54:07 +0800

    Site updated: 2023-05-23 22:53:51

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "1dfcdc",
+      "citySize": "16",
+      "aqiColor": "1dfcdc",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "15px 10px 10px 20px",
+      "shadow": "0",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "fd8dcc1d41b74407a0ef784e493e0ffb"
+    }
+  }
