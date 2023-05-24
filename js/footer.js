5d5d196832cede712c05afda52b6651d765d456f
TResuni <3026946953@qq.com>
2023-05-24 17:23:51 +0800

    Site updated: 2023-05-24 17:23:47

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 // 动态心跳，更改自己的名称
-$(document).ready(function(e){
-    $('.copyright').html('©2023 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i><span> By </span><a target="_blank" rel="noopener" href="https://tresuni.github.io/">TZYの博客</a>');
-})
+// $(document).ready(function(e){
+//     $('.copyright').html('©2023 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i><span> By </span><a target="_blank" rel="noopener" href="https://tresuni.github.io/">TZYの博客</a>');
+// })
 
 $(document).ready(function(e){
     show_date_time();
@@ -9,7 +9,7 @@ $(document).ready(function(e){
 
 //本站运行时间，更改自己建立站点的时间
 function show_date_time(){
-$('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span><div><span>框架 </span><a target="_blank" rel="noopener" href="https://hexo.io">Hexo</a><span> | </span><span>主题 </span><a target="_blank" rel="noopener" href="https://github.com/jerryc127/hexo-theme-butterfly">Butterfly</a></div>');
+$('.framework-info').html('<div>©2023 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i><span> By </span><a target="_blank" rel="noopener" href="https://tresuni.github.io/">TZYの博客</a></div>小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span><div><span>框架 </span><a target="_blank" rel="noopener" href="https://hexo.io">Hexo</a><span> | </span><span>主题 </span><a target="_blank" rel="noopener" href="https://github.com/jerryc127/hexo-theme-butterfly">Butterfly</a></div>');
 window.setTimeout("show_date_time()", 1000);
 BirthDay=new Date("5/18/2023 0:0:0");
 today=new Date();
